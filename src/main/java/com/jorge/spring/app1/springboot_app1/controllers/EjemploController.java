6082bba8532d422e06d9db17c150ba4984995875
Jorge <jorge.medi.08@gmail.com>
2025-12-28 19:51:08 -0600

    Pasar datos a un controlador

--- src/main/java/com/jorge/spring/app1/springboot_app1/controllers/EjemploController.java
+++ src/main/java/com/jorge/spring/app1/springboot_app1/controllers/EjemploController.java
@@ -1,7 +1,9 @@
 package com.jorge.spring.app1.springboot_app1.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 
@@ -9,9 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class EjemploController {
 
-
     @GetMapping("/detalles_info")
-    public String info() {
+    public String info(Model model) {
+        model.addAttribute("Titulo", "Servidor en linea");
+        model.addAttribute("Servidor", "Aplicacion Spring Boot funcionando correctamente");
+        model.addAttribute("IP", "192.168.1.1");
+
+        
         return "detalles_info";
     }
 }
